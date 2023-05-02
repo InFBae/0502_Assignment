e9e4cec41e0ab566a5c28952ea841b81ff9a18b1
JUNG BOBAE <acarid27@gmail.com>
2023-05-02 20:55:36 +0900

    feat: Add Dijkstra Class And Modify Assignment 2
    다익스트라 클래스 구현 및 과제 2번 수정

---
+++ 0502_Assignment/Dijkstra.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0502_Assignment
+{
+    internal class Dijkstra
+    {
+        public void ShortestPath(int start, int[,] graph, out int[] cost, out int[] path)
+        {
+            const int INF = 99999;
+            int vertex = graph.GetLength(0);
+            bool[] known = new bool[vertex];
+            cost = new int[vertex];
+            path = new int[vertex];
+
+            for (int i = 0; i < vertex; i++)
+            {
+                known[i] = false;
+                cost[i] = graph[start, i];
+                path[i] = graph[start, i] < INF ? start : -1;                
+            }
+            
+            path[start] = -1;
+            // 모든 정점을 검사 
+            for(int i = 0; i < vertex; i++)
+            {   
+                int next = -1;
+                int minCost = INF;
+                // 방문하지 않았으면서 가장 가까운 정점 찾기
+                for (int j = 0; j < vertex; j++)
+                {                    
+                    if (!known[j] && cost[j] < minCost)
+                    {
+                        next = j;
+                        minCost = cost[j];
+                    }
+                }
+                if (next < 0) break;
+                // j에 가장 가까운 거리 탐색  middle = next        
+                for(int j = 0; j < vertex; j++)
+                {
+                    if (cost[j] > cost[next] + graph[next, j])
+                    {
+                        cost[j] = cost[next] + graph[next, j];
+                        path[j] = next;
+                    }
+                }
+                known[next] = true;
+            }            
+        }
+    }
+}
--- 0502_Assignment/Program.cs
+++ 0502_Assignment/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int INF = 99999;
+            int[,] graph = new int[,]
+                {
+                    { 0 , 2 ,INF,INF, 9 ,INF,INF,INF},
+                    { 2 , 0 ,INF, 8 ,INF,INF, 5 ,INF},
+                    {INF,INF, 0 ,INF,INF,INF, 7 ,INF},
+                    {INF, 8 ,INF, 0 ,INF,INF,INF,INF},
+                    { 9 ,INF,INF,INF, 0 ,INF, 9 , 5 },
+                    {INF,INF,INF,INF,INF, 0 , 6 ,INF},
+                    {INF, 5 , 7 ,INF, 9 , 6 , 0 ,INF},
+                    {INF,INF,INF,INF, 5 ,INF,INF, 0 }
+                };
+
+            Dijkstra dijkstra = new Dijkstra();
+            int size = graph.GetLength(0);
+            int[] cost = new int[size];
+            int[] path = new int[size];
+            dijkstra.ShortestPath(0, graph, out cost, out path);
+
+            Console.WriteLine("Result: ");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Vertex : {i}\tCost : {cost[i]}\tPath : {path[i]}");
+            }
+
         }
     }
 }
